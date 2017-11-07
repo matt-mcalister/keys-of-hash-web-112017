fd19f793bbec1b090c84bf83c43d771b9e8322d1
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-07 21:31:15 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # binding.pry
+    result = []
+    each do |key, value|
+      if arguments.include?(value)
+        result << key
+      end
+    end
+    result
   end
 end
